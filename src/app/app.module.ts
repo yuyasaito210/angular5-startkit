e8d6bdb992a82e62908b187505fa3c94c889e65a
yuyasaito210 <yuyasaito210@gmail.com>
2018-03-29 09:07:33 -0700

    added fake signin procedure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,57 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers/index';
+
+import { AlertComponent } from './_directives/index';
+import { AuthGuard } from './_guards/index';
+import { JwtInterceptor } from './_helpers/index';
+import { AlertService, AuthenticationService, UserService } from './_services/index';
+import { HomeComponent } from './home/index';
 
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
+import { RegisterComponent } from './register/index';
+
+// import { LoginComponent } from './login/index';
 
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    routing
+  ],
   declarations: [
     AppComponent,
+    AlertComponent,
+    HomeComponent,
     LoginComponent,
-    UserComponent
+    RegisterComponent
   ],
-  imports: [
-    BrowserModule
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    },
+    // provider used to create fake backend
+    fakeBackendProvider
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { AlertService, AuthenticationService } from '../_services/index';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent implements OnInit {
+  model: any = {};
+  loading = false;
+  returnUrl: string;
+
+  constructor(
+  private route: ActivatedRoute,
+  private router: Router,
+  private authenticationService: AuthenticationService,
+  private alertService: AlertService) { }
 
   ngOnInit() {
+    // reset login status
+    this.authenticationService.logout();
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/images';
   }
 
+  login() {
+    this.loading = true;
+    if (this.authenticationService.fake_signin(this.model.email, this.model.password)) {
+      console.log(this.returnUrl);
+      this.router.navigate([this.returnUrl]);
+    } else {
+      this.alertService.error("user email or password is incorrect");
+      this.loading = false;
+    }
+  }
 }
